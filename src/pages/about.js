29c6d4affc9249cb84e855f845150a45ba5c9afa
Fabio Callegari <fabiocallegari.it@gmail.com>
2020-05-26 14:08:16 -0700

    add layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>About Me</h1>
     <p>My personal website</p>
     <Link to="/">&larr; back to home</Link>
-  </>
+  </Layout>
 );
